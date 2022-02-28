cd077f987095bf2ad5b1bc07b7e5bd7b9cc95a74
jean marinho <jeanmarinho529@gmail.com>
2022-02-28 03:14:10 -0300

    feature/ALD-8: criando novo metodo para formata cpf

--- app/Helpers/FormartDataHelper.php
+++ app/Helpers/FormartDataHelper.php
@@ -4,9 +4,14 @@ namespace App\Helpers;
 
 class FormartDataHelper
 {
-    public static function formartCnpj(string $param): string
+    public static function formartCnpj(?string $param): string
     {
         return preg_replace("/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/", "\$1.\$2.\$3/\$4-\$5", $param);
+    }
+
+    public static function formartCpf(?string $param): string
+    {
+        return preg_replace("/(\d{3})(\d{3})(\d{3})/", "\$1.\$2.\$3-", $param);
     }
 
     public static function formartPhone(?string $param): string
