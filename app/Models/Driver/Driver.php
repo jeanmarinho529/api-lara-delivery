b41f3e0642431b88c5a3074c5008ced69c9fff28
jean marinho <jeanmarinho529@gmail.com>
2022-02-27 22:51:08 -0300

    feature/ALD-5: incluindo sets no model de Driver para fazer um tratamento antes do insert no banco

--- app/Models/Driver/Driver.php
+++ app/Models/Driver/Driver.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Driver;
 
+use App\Helpers\ClearDataHelper;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -25,4 +26,14 @@ class Driver extends Model
     protected $casts = [
         'is_whatsapp' => 'boolean'
     ];
+
+    public function setPhoneAttribute(string $value): void
+    {
+        $this->attributes['phone'] = ClearDataHelper::clearAttribute($value);
+    }
+
+    public function setCpfAttribute(string $value): void
+    {
+        $this->attributes['cpf'] = ClearDataHelper::clearAttribute($value);
+    }
 }
